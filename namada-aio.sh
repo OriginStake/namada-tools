a2b6b49c58066754da7519c8898f99abe2c64f6c
tungdh1 <huy.tung@live.com>
2024-02-19 17:51:24 +0700

    Update the namada-aio.sh script to use the latest version of the

--- namada-aio.sh
+++ namada-aio.sh
@@ -15,7 +15,7 @@ function check_for_updates {
     latest_version=$(curl -s https://raw.githubusercontent.com/tungdh1/namada-tools/main/version.txt | tr -d '\r')
     
     # Check if the latest version is greater than the current version
-    if (( $(echo "$latest_version > $CURRENT_VERSION" | bc -l 2>/dev/null) )); then
+    if (( $(echo "$latest_version > $CURRENT_VERSION" | bc -l) )); then
         echo -e "${RED}A new version of the script is available. Would you like to update? (Yes/No)${NC}"
         read update_confirmation
         if [[ "${update_confirmation,,}" == "yes" ]]; then
