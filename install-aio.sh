6a48747c2b459bdbbbd4e87b5647c13f0cff1af6
tungdh1 <huy.tung@live.com>
2024-02-19 16:56:02 +0700

    Update the install-aio.sh script to use the new AIO repository URL. · 3e3e3e3e

--- install-aio.sh
+++ install-aio.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 # Download the content from the namada-aio.sh file and save it as namadaio
-curl -sL https://raw.githubusercontent.com/tungdh1/namada-tools/main/namada-aio.sh | sudo tee /usr/local/bin/namadaio > /dev/null && sudo 
-
-# Set execute permissions for the namadaio file
-chmod +x namadaio
-
-# Move the namadaio file to a common directory
-mv namadaio /usr/local/bin/
+curl -sL https://raw.githubusercontent.com/tungdh1/namada-tools/main/namada-aio.sh | sudo tee /usr/local/bin/namadaio > /dev/null && sudo chmod +x /usr/local/bin/namadaio
 
 echo "Finish install Namada All In One! Please run 'namadaio' to start using NamadaAIO Script."
