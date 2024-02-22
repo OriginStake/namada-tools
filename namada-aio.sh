5e9d1e6825b4b78abc2d9e004f3b20d485d121d2
tungdh1 <huy.tung@live.com>
2024-02-22 17:40:47 +0700

    Please enter the commit message for your changes. Lines starting

--- namada-aio.sh
+++ namada-aio.sh
@@ -8,7 +8,7 @@ NC='\033[0m' # No Color
 
 NEWCHAINID=shielded-expedition.88f17d1d14
 SCRIPT_NAME="namada-aio.sh"
-CURRENT_VERSION="1.3.2.8"
+CURRENT_VERSION="1.3.2.9"
 
 function manage_script {
     while true
@@ -177,7 +177,7 @@ function check_for_updates {
             sudo mv $SCRIPT_NAME /usr/local/bin/$NEW_SCRIPT_NAME
             sudo chmod +x /usr/local/bin/$NEW_SCRIPT_NAME
             echo "The script has been copied to /usr/local/bin/$NEW_SCRIPT_NAME."
-            exec "$(realpath \$0)" #Restart the script
+            exec "$NEW_SCRIPT_NAME" #Restart the script
         else
             echo "Update cancelled."
         fi
