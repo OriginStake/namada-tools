3d48bc249328ee01865e069baf1fab10d696ea03
tungdh1 <huy.tung@live.com>
2024-02-23 18:04:58 +0700

    Add des Join network

--- namada-aio.sh
+++ namada-aio.sh
@@ -10,7 +10,7 @@ NC='\033[0m' # No Color
 
 NEWCHAINID=shielded-expedition.88f17d1d14
 SCRIPT_NAME="namada-aio.sh"
-CURRENT_VERSION="1.3.4.3"
+CURRENT_VERSION="1.3.4.4"
 
 function manage_script {
     while true
@@ -149,6 +149,11 @@ function security_namada_menu {
 function join_namada_network_menu {
     while true
     do
+        echo "To join the Namada Network, please choose one of the following options:"
+        echo "- ${YELLOW}Genesis Validator${NC}: This role is for validators included in the Genesis file and can join/rejoin if listed in the Genesis list. If you are not selected in the Genesis list, this option will not be effective."
+        echo "- ${GREEN}Post Genesis Validator${NC}: This role is for validators/nodes that can join after the Genesis time starts (from Block 1 onwards). You can join Namada as a Validator at any time with this option."
+        echo "- ${BLUE}Full Node${NC}: If you only need to become a data node to serve queries from the Namada ledger or provide RPC/Indexer endpoints for DApps, this is the appropriate choice"
+        echo -e "\n"
         echo "Choose an option:"
         echo -e "1/ Join Namada as ${YELLOW}Genesis Validator${NC}"
         echo -e "2/ Join Namada as ${GREEN}Post Genesis Validator${NC}"
